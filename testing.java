5a0863233c489b2f3307fd70a891f005cd2f921c
WHS Bill <vsw@whs.school.nz>
2021-03-07 09:33:56 +1300

    test

--- testing.java
+++ testing.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class testing here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Bill
+ * @version 7-Mar-2021
  */
 
 // What is going on here?
